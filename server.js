4c9f1920e339f2b86bcfb11ad2332cdc2c18f67b
Dejan Draskovic <dejanx13@gmail.com>
2024-07-09 13:45:17 +0200

    minor changes

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-// Redirect /admin to /iso_chatbot/
-app.get('/admin', (req, res) => {
-  res.redirect('/iso_chatbot/');
-});
-
 // Serve static files from the dist directory
 app.use('/iso_chatbot', express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA (Single Page Application)
 app.get('/iso_chatbot/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Redirect all other routes to /iso_chatbot/
+app.get('*', (req, res) => {
+  res.redirect('/iso_chatbot/');
 });
 
 app.listen(port, () => {
